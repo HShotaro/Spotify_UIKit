fcbf3fa65e7d92ee767cd316e27a4f18c71b6c64
HShotaro9 <shotaro.10965@gmail.com>
2021-08-01 02:10:38 +0900

    プロジェクトを作成

---
+++ Music2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music2
+//
+//  Created by 平野翔太郎 on 2021/07/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
